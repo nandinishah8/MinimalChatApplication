b1ff0c49c4159e457a859a1a74163e4af950ea90
Nandini Shah <nandinishah8501@gmail.com>
2023-07-18 18:19:42 +0530

    Add project files.

---
+++ MinimalChatApplication/Data/MinimalChatContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalChatApplication.Models;
+using System.Collections.Generic;
+
+namespace MinimalChatApplication.Data
+{
+    // DataContext.cs
+    public class MinimalChatContext : DbContext
+    {
+        public MinimalChatContext(DbContextOptions<MinimalChatContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+        }
+        public DbSet<User> Users { get; set; }
+       
+
+    }
+
+}
---
+++ MinimalChatApplication/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalChatApplication.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+//configuring db path
+builder.Services.AddDbContext<MinimalChatContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("MinimalChatContext")));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+else
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<MinimalChatContext>();
+    context.Database.EnsureCreated();
+    // DbInitializer.Initialize(context);
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
